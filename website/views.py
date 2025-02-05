f8e0b519c5e94601de078eac0e3a996441375e23
akintolatechnologies <akintolatechnologies@gmail.com>
2025-02-05 18:35:25 +0100

    Landing implementation

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def landing(request):
+    return render(
+        request,
+        'account/onboarding.html',
+    )
--- zenithachievers/urls.py
+++ zenithachievers/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("account.urls")),
+    path('', include("website.urls")),
+    path('account/', include("account.urls")),
     path('admin/', admin.site.urls),
 ]
